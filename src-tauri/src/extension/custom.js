4c9c5bf71b36a778332aec7233880740fdb4849a
wjh <34159401+wjh360@users.noreply.github.com>
2025-01-14 18:12:42 +0800

    update custom js from pakeplus

--- src-tauri/src/extension/custom.js
+++ src-tauri/src/extension/custom.js
@@ -4,48 +4,43 @@
  * Additionally, you can directly include any script files in this file
  * that you wish to attach to the application.
  */
-console.log(
-    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-    'color:orangered;font-weight:bolder'
-)
+// console.log(
+//     '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
+//     'color:orangered;font-weight:bolder'
+// )
 
-document.addEventListener('DOMContentLoaded', () => {
-    const originalWindowOpen = window.open
-    window.open = function (url, _, features) {
-        return originalWindowOpen.call(window, url, '_self', features)
-    }
-    console.log('window.open has been overridden to open in the current page.')
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const originalWindowOpen = window.open;
+  window.open = function (url, _, features) {
+    return originalWindowOpen.call(window, url, "_self", features);
+  };
+  console.log("window.open has been overridden to open in the current page.");
+});
 
-document.addEventListener('DOMContentLoaded', () => {
-    const targetNode = document.body
-    // 配置观察选项
-    const config = {
-        childList: true,
-        subtree: true,
+document.addEventListener("DOMContentLoaded", () => {
+  const targetNode = document.body;
+  // 配置观察选项
+  const config = {
+    childList: true,
+    subtree: true,
+  };
+  const observer = new MutationObserver((mutationsList, observer) => {
+    let htmlContent = document.documentElement.innerHTML;
+    console.log("window.open has been overridden to open in the current page.");
+    for (const mutation of mutationsList) {
+      if (mutation.type === "childList" && htmlContent.includes("_blank")) {
+        const links = document.querySelectorAll('a[target="_blank"]');
+        links.forEach((link) => {
+          link.addEventListener("click", function (event) {
+            event.preventDefault();
+            window.location.href = link.href;
+          });
+        });
+      }
     }
-    const observer = new MutationObserver((mutationsList, observer) => {
-        let htmlContent = document.documentElement.innerHTML
-        console.log(
-            'window.open has been overridden to open in the current page.'
-        )
-        for (const mutation of mutationsList) {
-            if (
-                mutation.type === 'childList' &&
-                htmlContent.includes('_blank')
-            ) {
-                const links = document.querySelectorAll('a[target="_blank"]')
-                links.forEach((link) => {
-                    link.addEventListener('click', function (event) {
-                        event.preventDefault()
-                        window.location.href = link.href
-                    })
-                })
-            }
-        }
-    })
-    observer.observe(targetNode, config)
-})
+  });
+  observer.observe(targetNode, config);
+});
 // tem.js
 document.addEventListener("DOMContentLoaded", function () {
   // 获取当前时间戳
