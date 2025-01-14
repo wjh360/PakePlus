a47b91975f6da60a81b1ac34a1b3081e44a1eeed
wjh <34159401+wjh360@users.noreply.github.com>
2025-01-14 18:04:34 +0800

    Update custom.js

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -4,10 +4,10 @@
  * Additionally, you can directly include any script files in this file
  * that you wish to attach to the application.
  */
-console.log(
-    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-    'color:orangered;font-weight:bolder'
-)
+// console.log(
+//     '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
+//     'color:orangered;font-weight:bolder'
+// )
 
 document.addEventListener('DOMContentLoaded', () => {
     const originalWindowOpen = window.open
